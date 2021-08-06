425e7199256d8ad5aea2b607f9cb2b9261896779
vapaus <snaqerou@gmail.com>
2021-08-06 23:14:55 +0200

    order book websocket new subscription

--- src/hooks/useOrderBook.tsx
+++ src/hooks/useOrderBook.tsx
@@ -2,9 +2,11 @@ import { useEffect, useRef, useState } from "react";
 import { Action } from "types/Actions";
 import { Order } from "types/Order";
 import { TradingPair } from "types/TradingPair";
+import { TradingStats } from "types/TradingStats";
 import {
   tradingOrderBookLimitedProxy,
   tradingOrderBookSubscribePublic,
+  tradingStatsProxy,
 } from "utils/subscriptionAction";
 import { w3cwebsocket as W3CWebSocket } from "websocket";
 
@@ -15,6 +17,7 @@ const useOrderBook = () => {
     TradingPair.btcPln
   );
   const [orders, setOrders] = useState<Orders>({ buy: [], sell: [] });
+  const [stats, setStats] = useState<TradingStats>();
   const webSocket = useRef<W3CWebSocket | null>(null);
 
   useEffect(() => {
@@ -24,7 +27,7 @@ const useOrderBook = () => {
       webSocket.current?.send(
         tradingOrderBookSubscribePublic(activeTradingPair)
       );
-      webSocket.current?.send(tradingOrderBookLimitedProxy(activeTradingPair));
+      webSocket.current?.send(tradingStatsProxy(activeTradingPair));
     };
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -39,7 +42,13 @@ const useOrderBook = () => {
       const data = JSON.parse(message.data as string);
 
       if (data?.action === Action.proxyResponse) {
-        setOrders({ buy: data.body.buy || [], sell: data.body.sell || [] });
+        if (data.body?.buy) {
+          setOrders({ buy: data.body.buy || [], sell: data.body.sell || [] });
+        }
+
+        if (data.body?.stats) {
+          setStats(data.body.stats);
+        }
       }
 
       if (data?.action === Action.push) {
@@ -54,6 +63,7 @@ const useOrderBook = () => {
     webSocket,
     activeTradingPair,
     setActiveTradingPair,
+    stats,
     orders,
     setOrders,
   };
--- src/utils/subscriptionAction.ts
+++ src/utils/subscriptionAction.ts
@@ -12,18 +12,43 @@ const tradingOrderBookSubscribePublic = (tradingPair: TradingPair) =>
   JSON.stringify({
     action: "subscribe-public",
     module: "trading",
-    path: `orderbook/${tradingPair}`,
+    path: `orderbook-limited/${tradingPair}/10`,
   });
 
 const tradingOrderBookUnsubscribe = (tradingPair: TradingPair) =>
   JSON.stringify({
     action: "unsubscribe",
     module: "trading",
-    path: `orderbook/${tradingPair}`,
+    path: `orderbook-limited/${tradingPair}/10`,
+  });
+
+const tradingStatsProxy = (tradingPair: TradingPair) =>
+  JSON.stringify({
+    requestId: "78539fe0-e9b0-4e4e-8c86-70b36aa93d4f",
+    action: "proxy",
+    module: "trading",
+    path: `stats/${tradingPair}`,
+  });
+
+const tradingStatsSubscribePublic = (tradingPair: TradingPair) =>
+  JSON.stringify({
+    action: "subscribe-public",
+    module: "trading",
+    path: `stats/${tradingPair}`,
+  });
+
+const tradingStatsUnSubscribePublic = (tradingPair: TradingPair) =>
+  JSON.stringify({
+    action: "unsubscribe",
+    module: "trading",
+    path: `stats/${tradingPair}`,
   });
 
 export {
   tradingOrderBookLimitedProxy,
   tradingOrderBookSubscribePublic,
   tradingOrderBookUnsubscribe,
+  tradingStatsProxy,
+  tradingStatsSubscribePublic,
+  tradingStatsUnSubscribePublic,
 };
